f8ac281154b6cbc7a047263f9353a0c68ba5ee60
Jakub Uryga <urygajakub@gmail.com>
2025-02-10 13:24:14 +0100

    selenium Update

--- test1/Program.cs
+++ test1/Program.cs
@@ -18,9 +18,19 @@ namespace test1
         {
             DictionaryManagment dictionaryInstance = DictionaryManagment.CreateInstance();
 
-            User Uryga = new User();
+            User Uryga = new User("1PTP335786", "mdnnb");
             ChromeBot UrygaSession = new ChromeBot(Uryga, 2000, dictionaryInstance);
             UrygaSession.TakeTheQuiz();
+
+            User Wańczyk = new User("aktr18916", "rkned");
+            ChromeBot WańczykSession = new ChromeBot(Wańczyk, 2000, dictionaryInstance);
+            WańczykSession.TakeTheQuiz();
+
+
+            User Gocal = new User("1PTP70798", "eicrw");
+            ChromeBot GocalSession = new ChromeBot(Gocal, 2000, dictionaryInstance);
+            GocalSession.TakeTheQuiz();
+
 
 
             Console.WriteLine("WORDS DICTIONARY (HI HITLA!)");
